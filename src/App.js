c694312e2db5ad8bda3bc9cd9c0cfe947df899d0
climax-solution <blocklimax@gmail.com>
2022-04-11 14:17:54 +0300

    init project and add parallax banner

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SliderParallax from './components/parallax';
+
+function App() {
+  return (
+    <div className="App">
+      <SliderParallax/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/parallax.js
@@ -0,0 +1,56 @@
+import { Parallax } from "react-scroll-parallax";
+
+const parallax = [
+    {
+        'class': "layer-floor",
+        'effect' : {
+            scaleY: [-1, 1]
+        }
+    },
+    {
+        'class': "layer-4",
+        'effect' : {
+            translateY: ["0px","480px"]
+        }
+    },
+    {
+        'class': "layer-3",
+        'effect' : {
+            translateY: ["0px","202px"]
+        }
+    },
+    {
+        'class': "layer-2",
+        'effect' : {
+            translateY: ["0px","-125px"]
+        }
+    },
+    {
+        'class': "layer-1",
+        'effect' : {
+            translateY: ["0px","-480px"]
+        }
+    },
+    {
+        'class': "layer-0",
+        'effect' : {
+            translateY: ["0px","-940px"]
+        }
+    },
+];
+
+export default function SliderParallax() {
+    return (
+        <div className="parallax-container">
+            {
+                parallax.map((item, idx) => (
+                    <Parallax
+                        className={`parallax ${item.class}`}
+                        key={idx}
+                        {...item.effect}
+                    />
+                ))
+            }
+        </div>
+    )
+}

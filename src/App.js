7d3f9e89733014cef47f62ee01071dc9187864ee
climax-solution <blocklimax@gmail.com>
2022-04-11 14:55:41 +0300

    add brand

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Brand from './components/brand';
 import SliderParallax from './components/parallax';
 
 function App() {
   return (
     <div className="App">
+      <Brand/>
       <SliderParallax/>
     </div>
   );
---
+++ src/components/brand/index.js
@@ -0,0 +1,18 @@
+import brandLogo from "./img/brand.png";
+import "./style.css";
+
+export default function Brand() {
+    return (
+        <div className="brand-container">
+            <div className="logo">
+                <img src={brandLogo} alt=""/>
+            </div>
+            <h3 className="brand-title text-center">
+                XRP: The Best Digital Asset for
+                <span className="d-block">Global Payments</span>
+            </h3>
+            <p className="brand-content text-center">XRP offers financial institutions the fastest, most reliable option for <span className="d-block">sourcing liquidity on demand.</span></p>
+            <a className="btn btn-primary btn-brand">Learn About On-Demand Liquidity</a>
+        </div>
+    )
+}

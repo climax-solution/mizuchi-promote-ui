69374ac5dc30e1141af3f895cbe8695dfed46249
climax-solution <blocklimax@gmail.com>
2022-04-11 15:49:04 +0300

    add comp3

---
+++ src/components/about/comp3.js
@@ -0,0 +1,17 @@
+export default function Comp3() {
+    return (
+        <div
+        >
+            <div className="row">
+                <div className="col-md-6 col-12">
+                    <h1>RippleNet</h1>
+                    <p>RippleNet customers can use XRP for sourcing liquidity in cross-border transactions, instead of pre-funding—ensuring instant settlement, lower exchange fees and more efficient use of working capital.</p>
+                    <button className="btn btn-primary">Leran About RippleNet</button>
+                </div>
+                <div className="col-md-6 col-12 position-relative">
+                    <div className="ripple-net"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,5 +1,6 @@
 import Comp1 from "./comp1";
 import Comp2 from "./comp2";
+import Comp3 from "./comp3";
 import "./style.css";
 
 export default function About() {
@@ -8,6 +9,7 @@ export default function About() {
             <div className="container">
                 <Comp1/>
                 <Comp2/>
+                <Comp3/>
             </div>
         </div>
     )
